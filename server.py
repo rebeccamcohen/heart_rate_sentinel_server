e81a59019f872377f333bd767880096b3d1e5d26
Rebecca Cohen <rebecca.cohen@duke.edu>
2018-11-16 15:19:06 -0500

    commented out loop back (host=127.0.0.1)

--- server.py
+++ server.py
@@ -235,5 +235,5 @@ def internal_average():
     return jsonify(internal_avg)
 
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1")
+#if __name__ == "__main__":
+ #   app.run(host="127.0.0.1")
